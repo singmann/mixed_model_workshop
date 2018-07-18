a01faf1bee7185e9187c30a1eb0340680d969198
Henrik Singmann <singmann@gmail.com>
2018-07-18 18:29:12 +0200

    redid mixed model session

--- exercises/exercise_2_SOLUTION.R
+++ exercises/exercise_2_SOLUTION.R
@@ -1,44 +1,4 @@
 ## ------------------------------------------------------------------------
-require(afex)
-load("ssk16_dat_preapred.rda") # data preapred in 'prepare_data.R'
-str(dat)
-# 'data.frame':	2196 obs. of  8 variables:
-# $ p_id       : Factor w/ 183 levels "102_P(if,then)",..: 63 63 63 63 63 63 63 63 63 63 ...
-# $ i_id       : Factor w/ 12 levels "1","2","3","4",..: 1 2 3 4 5 6 7 8 9 10 ...
-# $ CgivenA    : num  52 60 6 0 8 8 79 0 51 79 ...
-# $ DV         : num  52 1 5 0 7 0 84 0 51 94 ...
-# $ rel_cond   : Factor w/ 3 levels "PO","NE","IR": 1 3 2 2 2 2 1 3 3 1 ...
-# $ dv_question: Factor w/ 2 levels "probability",..: 1 1 1 1 1 1 1 1 1 1 ...
-# $ dv         : num  0.02 -0.49 -0.45 -0.5 -0.43 -0.5 0.34 -0.5 0.01 0.44 ...
-# $ c_given_a  : num  0.02 0.1 -0.44 -0.5 -0.42 -0.42 0.29 -0.5 0.01 0.29 ...
-
-## ---- eval=FALSE---------------------------------------------------------
-## mixed(dv ~ ...)
-
-m_full <- mixed(dv ~ c_given_a*rel_cond*dv_question +
-                  (rel_cond*c_given_a||p_id) +
-                  (rel_cond*c_given_a*dv_question||i_id),
-                dat, expand_re = TRUE,
-                control = lmerControl(optCtrl = list(maxfun=1e8)),
-                method = "S")
-m_full
-# Mixed Model Anova Table (Type 3 tests, S-method)
-# 
-# Model: dv ~ c_given_a * rel_cond * dv_question + (rel_cond * c_given_a || 
-# Model:     p_id) + (rel_cond * c_given_a * dv_question || i_id)
-# Data: dat
-# Effect        df           F p.value
-# 1                      c_given_a 1, 190.51 1091.33 ***  <.0001
-# 2                       rel_cond  2, 23.40  163.62 ***  <.0001
-# 3                    dv_question  1, 53.87        0.02     .88
-# 4             c_given_a:rel_cond  2, 27.45   39.73 ***  <.0001
-# 5          c_given_a:dv_question  1, 19.83        0.00     .95
-# 6           rel_cond:dv_question  2, 29.67        2.20     .13
-# 7 c_given_a:rel_cond:dv_question 2, 280.91      2.63 +     .07
-# ---
-#   Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '+' 0.1 ' ' 1
-
-## ------------------------------------------------------------------------
 data("fhch2010", package = "afex")
 str(fhch2010)
 # 'data.frame':	13222 obs. of  10 variables:
